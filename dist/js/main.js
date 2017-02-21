4ac49af8927124a2b0a17e13a72a30fafd26a02f
Paval Miatlitski <paval.escoba@gmail.com>
2017-02-21 23:04:07 +0300

    Add files via upload
    added some new data

---
+++ dist/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	(function(){
+		$('.about__desc').perfectScrollbar();
+	}());
+	(function(){
+		$('.edu').perfectScrollbar();
+	}());
+	
+	(function(){
+		$('.tabs-link').on('click', function(e){
+			e.preventDefault();
+			var $this 	= $(this),
+					item 		= $this.closest('.tabs-item'),
+					list 		= $this.closest('.tabs'),
+					links		= list.find('.tabs-link'),				
+					content = $('.tabs-content'),
+					itemPos = item.data('class');
+
+				content.filter('.tabs-content-' + itemPos)
+					.add(item)
+					.addClass('active')
+					.siblings()
+					.removeClass('active');
+		});
+	}());	
+});
